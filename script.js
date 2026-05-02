ff52f163e54545fb23498f39cd645aa216933a9b
Dihini Sehanga <dihinisehanga29@gmail.com>
2026-05-02 13:22:39 +0530

    create dashboard

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 function goToDashboard() {
-    // values ලබා ගැනීම
+
     const userValue = document.getElementById('username').value;
     const passValue = document.getElementById('password').value;
 
     const loginSection = document.getElementById('login-container');
     const dashSection = document.getElementById('dashboard-container');
 
-    // Username සහ Password පරීක්ෂා කිරීම
     if (userValue === "admin" && passValue === "1234") {
-        // Login එක සඟවා Dashboard එක පෙන්වීම
         loginSection.style.display = "none";
-        dashSection.style.display = "block"; // flex එක වෙනුවට block හෝ flex පාවිච්චි කළ හැක
+        dashSection.style.display = "block";
 
-        // උඩටම scroll කිරීම
         window.scrollTo(0, 0);
     } else {
-        alert("වැරදි දත්ත! කරුණාකර නැවත උත්සාහ කරන්න.");
+        alert("email or password wrong please try again!");
     }
 }
 
 function goToLogin() {
-    // Logout වීමේදී Dashboard සඟවා Login පෙන්වීම
     document.getElementById('dashboard-container').style.display = "none";
     document.getElementById('login-container').style.display = "flex";
 
-    // Form එක reset කිරීම
     document.getElementById('loginForm').reset();
+}
+
+function goToDashboard(){
+    document.getElementById("login-container").classList.add("d-none");
+    document.getElementById("dashboard-container").classList.remove("d-none");
+}
+
+function goToLogin(){
+    document.getElementById("dashboard-container").classList.add("d-none");
+    document.getElementById("login-container").classList.remove("d-none");
 }
